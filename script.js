2f3095c134111f3d7f881bf76b83137c2cf795dc
louisefogelqvist <louise.fogelqvist@hotmail.com>
2023-10-31 10:43:05 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+// <ul>
+// <li><a href='index.html'>Startsidan</a></li>
+// <li><a href='annansida.html'>Annan sida</a></li>
+// </ul>
+
+function buildMenu()
+{
+    var links = [
+        ["Startsidan", "index.html"],
+        ["Hitta hit", "hittahit.html"],
+        ["Om oss", "om.html"],
+        ["Kontakta oss", "kontakt.html"]
+        
+    ]
+
+    var menyHtml = ""
+    for(var i = 0; i < links.length; i = i + 1)
+    {
+        menyHtml += "<li><a href='" + links[i][1] + "'>" + links[i][0] + "</a></li>"
+    }
+    document.getElementById("meny").innerHTML = menyHtml
+
+}
